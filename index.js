3c2857e1c460820695c7118e515083f77fad6015
saramccombs <theSaraMcCombs@gmail.com>
2020-03-08 13:27:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+//Remove main#main
+document.querySelector('main#main').remove();
+
+//Create new element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Sara is the champion';
